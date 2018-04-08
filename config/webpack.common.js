269456709665db160934823cc64e1c80331b1225
qiwang@ixiaopu.com <qiwang@ixiaopu.com>
2018-04-08 11:15:41 +0800

    初始化

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -33,7 +33,8 @@ module.exports = {
 
 
         new HtmlWebpackPlugin({
-            template: 'src/index.html'
+            template: 'src/index.html',
+            minify: {collapseWhitespace: true}
         })
     ]
 }
